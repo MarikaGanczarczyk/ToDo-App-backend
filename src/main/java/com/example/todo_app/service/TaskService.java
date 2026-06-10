6c3abbf9fb519d3efd20acfbae852c779d7476d5
marik1504894 <marik1504894@Mastek.com>
2026-06-10 11:37:51 +0100

    created get-all, get-all-by-Id & Update

--- src/main/java/com/example/todo_app/service/TaskService.java
+++ src/main/java/com/example/todo_app/service/TaskService.java
@@ -5,18 +5,41 @@ import com.example.todo_app.repository.TaskRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TaskService {
 
     @Autowired
-    private TaskRepo repo ;
+    private TaskRepo repo;
 
-    public Task addTask (Task task){
+    public Task addTask(Task task) {
         return repo.save(task);
     }
 
 
+    public List<Task> getAllTasks() {
+        return repo.findAll();
+    }
+
+    public Task getTaskById(int id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Task not found: " + id));
+    }
+
+    public Task updateTaskById(int id, Task updatedTask) {
+
+
+        Task existingTask = repo.findById(id).orElseThrow(() -> new RuntimeException("Task not found: " + id));
+        existingTask.setTitle(updatedTask.getTitle());
+        existingTask.setDescription(updatedTask.getDescription());
+        existingTask.setPriority(updatedTask.getPriority());
+        existingTask.setStatus(updatedTask.getStatus());
+        existingTask.setDueDate(updatedTask.getDueDate());
+
+        return repo.save(existingTask);
 
 }
 
+}
 
